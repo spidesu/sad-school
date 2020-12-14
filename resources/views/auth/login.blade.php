9b8e976c36fc0afb81c878235507db1baceead7d
fedomikhail <fedomikhail@gmail.com>
2020-12-14 12:00:22 +0300

    second commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,31 +16,31 @@
             @csrf
 
             <div>
-                <x-jet-label for="username" value="{{ __('Username') }}" />
+                <x-jet-label for="username" value="Логин" />
                 <x-jet-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-label for="password" value="Пароль" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <input id="remember_me" type="checkbox" class="form-checkbox" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">Запомнить</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        Забыли пароль?
                     </a>
                 @endif
 
                 <x-jet-button class="ml-4">
-                    {{ __('Login') }}
+                    Войти
                 </x-jet-button>
             </div>
         </form>
